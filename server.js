5d06f45609d29ae42f8c56c0ff78b589f520b0fe
versusx <armannet123@hotmail.com>
2017-10-18 19:45:28 +0500

    first app

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 "use strict";
 
 const http = require('http');
+const fs = require('fs');
 
 http.createServer(function (req, res) {
-   res.end();
+   var html = fs.readFileAsync('index.html', 'utf-8');
+   res.end(html);
 }).listen(process.env.PORT);
 
 console.log("Serer started");
